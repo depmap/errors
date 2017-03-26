44ea14f5926cd6c89e392806f94e5e871bbb0029
Henry Snopek <hhsnopek@gmail.com>
2017-03-26 17:02:05 -0400

    add color via chalk, improve error, remove log

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = function (err) {
-  console.error(err)
+const chalk = require('chalk')
+module.exports = { error, warn }
+
+function error (error) {
+  let tmp = error.stack.split('\n')
+  let stack = { msg: tmp[0] }
+  tmp.shift()
+  stack.trace = tmp.join('\n')
+
+  // only print error message if it's not in the stack message
+  if (stack.msg.indexOf(error.msg) > -1) console.log(chalk.red(error.message))
+  console.log(chalk.red(stack.msg))
+  console.log(chalk.gray(stack.trace))
+
+  process.exit(1)
 }
+
+function warn (warning) { console.log(chalk.yellow(warning)) }
